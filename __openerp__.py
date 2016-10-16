18e3c3107cbedb25480472a2d43fb95d1d0b5609
root <root@saidi-VirtualBox.(none)>
2016-10-16 20:55:50 +0100

    first

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  
+##############################################################################
+{
+	'name': 'CheckBox toggle',
+	'version': '1.0.0',
+	'category': 'sale',
+	'author': 'Saidi Oussama',
+	'website': '',
+	'depends': ['web'],
+	'data' : [
+			"checkbox_toggle.xml",
+	],
+	'qweb':["static/src/xml/checkbox_toggle.xml"],
+	 
+
+	'auto_install': False,
+}
+
---
+++ static/src/js/checkbox_toggle.js
@@ -0,0 +1,21 @@
+﻿openerp.checkbox_toggle = function (instance,local) {
+    var _t = instance.web._t,
+        _lt = instance.web._lt;
+    var QWeb = instance.web.qweb;
+
+    local.ToggleWidget = instance.web.form.FieldBoolean.extend({
+        template: "ToggleWidgetTemplate",
+       
+
+        start: function () {
+            return this._super();
+        },
+       
+
+       
+
+        
+    });
+
+    instance.web.form.widgets.add('toggle_but', 'instance.checkbox_toggle.ToggleWidget');
+}
